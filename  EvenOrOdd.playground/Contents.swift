cd31e2b488e6c9e32a303e81816dfa27e79f4fb9
Egor Gorskikh <egorgorskikh@gmail.com>
2021-09-16 18:36:48 +0300

    feat: Even or Odd

---
+++  EvenOrOdd.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+/*
+ Swift.
+ Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+ */
+
+func evenOrOdd(_ number: Int) -> String {
+    number % 2 == 0  ? "Even" : "Odd"
+}
