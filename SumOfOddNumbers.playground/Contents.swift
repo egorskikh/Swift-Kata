b46389abd3dc815d0d570e135cb533c80bc7087e
Egor Gorskikh <egorgorskikh@gmail.com>
2021-10-24 18:08:05 +0300

    feat: Sum of odd numbers

---
+++ SumOfOddNumbers.playground/Contents.swift
@@ -0,0 +1,10 @@
+/*
+ Swift.
+
+ Given the triangle of consecutive odd numbers:
+ Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+ */
+
+func rowSumOddNumbers(_ row: Int) -> Int {
+    return row * row * row
+}
