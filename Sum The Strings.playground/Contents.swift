908655ae52cc268f394d3aa73ad7b4d840d69e4c
Egor Gorskikh <egorgorskikh@gmail.com>
2021-11-14 13:55:04 +0300

    feat: Sum The Strings

---
+++ Sum The Strings.playground/Contents.swift
@@ -0,0 +1,10 @@
+/*
+ Swift.
+
+ Create a function that takes 2 integers in form of a string as an input,
+ and outputs the sum (also as a string).
+ */
+
+func sum_str(_ a: String, _ b: String) -> String {
+    String( (Int(a) ?? 0) + (Int(b) ?? 0) )
+}
