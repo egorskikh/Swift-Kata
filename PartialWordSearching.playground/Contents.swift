24c45c625d9375f62c1cf20b0825f0773f1389e0
Egor Gorskikh <egorgorskikh@gmail.com>
2021-12-15 20:23:23 +0300

    feat: Partial Word Searching

--- PartialWordSearching.playground/Contents.swift
+++ PartialWordSearching.playground/Contents.swift
@@ -1,15 +1,11 @@
 import UIKit
 
 /*
- Swift.
-
- Write a method that will search an array of strings for all strings that contain another string,
- ignoring capitalization. Then return an array of the found strings.
-
- The method takes two parameters, the query string and the array of strings to search, and returns an array.
+ Partial Word Searching. Swift.
  */
 
 func wordSearch(_ str:String, _ arr:[String]) -> [String] {
-    let array = arr.filter { $0.lowercased().range(of: str.lowercased()) != nil }
-    return array.isEmpty ? ["Empty"] : array
+    let results = arr.filter { $0.localizedCaseInsensitiveContains(str) }
+    return results.isEmpty ? ["Empty"] : results
 }
+
