55921918fa0e79fc683dc33bae2dd3d66681258d
Egor Gorskikh <egorgorskikh@gmail.com>
2021-12-23 22:42:46 +0300

    feat: Disarium Number (Special Numbers Series #3)

---
+++ Disarium Number (Special Numbers Series #3).playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+// Disarium Number (Special Numbers Series #3). Swift.
+
+func disariumNumber(_ number: Int) -> String {
+    let numberArr = String(number).compactMap { Int(String($0)) }
+    let ret = numberArr.enumerated().map { (i, n) in Int(pow(Double(n), Double(i + 1))) }.reduce(0, +)
+    return ret == number ? "Disarium !!" : "Not !!"
+}
