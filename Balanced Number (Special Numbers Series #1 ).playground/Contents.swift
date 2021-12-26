e84dfb52aaeae5e75cc88ea49a47d4b82ad77a51
Egor Gorskikh <egorgorskikh@gmail.com>
2021-12-26 20:54:59 +0300

    refactoring: Balanced Number (Special Numbers Series #1 )

--- Balanced Number (Special Numbers Series #1 ).playground/Contents.swift
+++ Balanced Number (Special Numbers Series #1 ).playground/Contents.swift
@@ -6,36 +6,53 @@ func balancedNumber(_ number: Int) -> String {
     var returnString = "Balanced"
     let countInt = String(number).count
     
-    if countInt == 1 || countInt == 2 { return returnString }
+    if countInt < 2 { return returnString }
     
-    let intArray = String(number).compactMap{Int(String($0))}
-    let midIndex = countInt / 2
-    var checkIndex = 0
+    let intNumberArray: [Int] = String(number).compactMap{ $0.wholeNumberValue }
+    let midIndex: Int = countInt % 2 == 0 ? (countInt / 2) - 1 : (countInt / 2)
     
-    var sumLeftSide = 0
-    var sumRightSide = 0
-
-    if countInt % 2 == 0 {
-        for i in intArray {
-            if checkIndex < (midIndex - 1) {
-                sumLeftSide += i
-            } else if checkIndex > midIndex {
-                sumRightSide += i
-            }
-            checkIndex += 1
-        }
-    } else {
-        for i in intArray {
-            if checkIndex < midIndex {
-                sumLeftSide += i
-            } else if checkIndex > midIndex {
-                sumRightSide += i
-            }
-            checkIndex += 1
-        }
-    }
+    let sumLeftSide = intNumberArray.prefix(midIndex).reduce(0, +)
+    let sumRightSide = intNumberArray.suffix(midIndex).reduce(0, +)
     
     returnString = sumLeftSide != sumRightSide ? "Not Balanced" : "Balanced"
     return returnString
 }
+
+//func balancedNumber(_ number: Int) -> String {
+//    var returnString = "Balanced"
+//    let countInt = String(number).count
+//
+//    if countInt == 1 || countInt == 2 { return returnString }
+//
+//    let intArray = String(number).compactMap{Int(String($0))}
+//    let midIndex = countInt / 2
+//    var checkIndex = 0
+//
+//    var sumLeftSide = 0
+//    var sumRightSide = 0
+//
+//    if countInt % 2 == 0 {
+//        for i in intArray {
+//            if checkIndex < (midIndex - 1) {
+//                sumLeftSide += i
+//            } else if checkIndex > midIndex {
+//                sumRightSide += i
+//            }
+//            checkIndex += 1
+//        }
+//    } else {
+//        for i in intArray {
+//            if checkIndex < midIndex {
+//                sumLeftSide += i
+//            } else if checkIndex > midIndex {
+//                sumRightSide += i
+//            }
+//            checkIndex += 1
+//        }
+//    }
+//
+//    returnString = sumLeftSide != sumRightSide ? "Not Balanced" : "Balanced"
+//    return returnString
+//}
+
 
