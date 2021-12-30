4c839d7d7ed984b5ffe8ec595d63b73127f4fb7a
Egor Gorskikh <egorgorskikh@gmail.com>
2021-12-31 02:23:55 +0300

    refactoring: Irreducible Sum of Rationals

--- Waiting for criticism/Irreducible Sum of Rationals.playground/Contents.swift
+++ Waiting for criticism/Irreducible Sum of Rationals.playground/Contents.swift
@@ -3,19 +3,19 @@ import UIKit
 // Irreducible Sum of Rationals. Swift.
 
 /*
-   testing([(1, 2), (1, 3), (1, 4)], (13, 12))
-   testing([(1, 3), (5, 3)], (2, 1))
-   testingNil([])
-   testing([(1, 8), (1, 9)], (17, 72))
+ testing([(1, 2), (1, 3), (1, 4)], (13, 12))
+ testing([(1, 3), (5, 3)], (2, 1))
+ testingNil([])
+ testing([(1, 8), (1, 9)], (17, 72))
  */
 
 func sumFracts(_ l: [(Int, Int)]) -> (Int, Int)? {
-
+    
     if l.isEmpty { return nil }
-
+    
     var numerator = 0
     var denominator = 0
-
+    
     // We get the sum of fractions
     for i in l {
         if numerator == 0 && denominator == 0 {
@@ -26,20 +26,23 @@ func sumFracts(_ l: [(Int, Int)]) -> (Int, Int)? {
             denominator = denominator * i.1
         }
     }
-
-    // Find node
-    let setFindNode = euclidsAlgorithm(numerator, denominator)
-    let node = setFindNode.1
     
-    // Divide into node
-    return (numerator / node, denominator / node)
+    // Find gcd
+    let setFindNode = euclidsAlgorithm(numerator, denominator)
+    let gcd = setFindNode.1
+    
+    // Divide into gcd
+    return (numerator / gcd, denominator / gcd)
 }
 
-func euclidsAlgorithm(_ n1: Int, _ n2: Int) -> (Int, Int) {
-    if n1 % n2 == 0 { return (n1, n2) }
-    let remainder = n1 % n2
-    if remainder == 0 { (remainder,n2) }
-    return euclidsAlgorithm(n2, remainder)
+func euclidsAlgorithm(_ numerator: Int, _ denominator: Int) -> (Int, Int) {
+    
+    if numerator % denominator == 0 {
+        // denominator - this gcd
+        return (numerator, denominator)
+    }
+    let remainder = numerator % denominator
+    return euclidsAlgorithm(denominator, remainder)
 }
 
 sumFracts([(1, 2), (1, 3), (1, 4)]) // 13 12
