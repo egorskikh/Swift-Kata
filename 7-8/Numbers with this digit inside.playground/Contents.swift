bc213a0bf4dd5da5979ac1929d65f6c28ecaaef2
Egor Gorskikh <egorgorskikh@gmail.com>
2022-01-11 21:27:35 +0300

    feat: Numbers with this digit inside

---
+++ 7-8/Numbers with this digit inside.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+// Numbers with this digit inside. Swift
+
+func numbersWithDigitInside(_ x: Int64, _ d: Int64) -> [Int64] {
+    let numbers = (1...x).filter {String($0).contains(String(d))}
+    
+    return [Int64(numbers.count),
+            numbers.reduce(Int64(0), +),
+            numbers.count > 0 ? numbers.reduce(Int64(1), *) : Int64(0)]
+}
+
+
