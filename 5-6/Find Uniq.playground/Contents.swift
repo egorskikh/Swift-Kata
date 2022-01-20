86ba6f1bc0bbf0fe62645c42e96f971733af43ca
Egor Gorskikh <egorgorskikh@gmail.com>
2022-01-20 20:18:19 +0300

    feat: Find the unique number

---
+++ 5-6/Find Uniq.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+// Find the unique number. Swift.
+
+func findUniq(_ arr: [Double]) -> Double {
+    Set(arr).filter {
+        arr.firstIndex(of: $0) == arr.lastIndex(of: $0)
+    }.first!
+}
