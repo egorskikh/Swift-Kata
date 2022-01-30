7d3b60a3b072f21b426a590ca77fb46db94ba87f
Yegor Gorskikh <yegorskikh@gmail.com>
2022-01-31 01:39:13 +0300

    init: start Find the nth Digit of a Number

---
+++ 7-8/Find the nth Digit of a Number.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+// Find the nth Digit of a Number. Swift.
+
+/*
+ 5673, 4 --> 5
+ 129, 2 --> 2
+ -2825, 3 --> 8
+ -456, 4 --> 0
+ 0, 20 --> 0
+ 65, 0 --> -1
+ 24, -8 --> -1
+ */
+
+func findDigit(_ num: Int, _ nth: Int) -> Int {
+    return 0
+}
