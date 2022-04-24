997fc2c371256467a67042cbde71ab5161ef2ea2
Yegor Gorskikh <egorgorskikh@gmail.com>
2022-04-24 17:38:34 +0300

    feat: Which are in?

---
+++ 5-6/WhichAreIn?.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+// Which are in?. Swift.
+
+func inArray(_ a1: [String], _ a2: [String]) -> [String] {
+    return Set(a1.filter { s1 in a2.contains { $0.contains(s1) } }).sorted()
+}
