a3f66377776f5bc2d6e54711bdaaa4da41e362f5
Yegor Gorskikh <egorgorskikh@gmail.com>
2022-05-31 20:05:11 +0300

    feat: Counting Duplicates

---
+++ 5-6/Counting Duplicates.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+// Counting Duplicates. Swift.
+
+func countDuplicates(_ s:String) -> Int {
+    var counts: [String: Int] = [:]
+    for character in Array(s) {
+        counts[character.lowercased(), default: 0] += 1
+    }
+    return counts.values.filter{ $0 > 1 }.count
+}
