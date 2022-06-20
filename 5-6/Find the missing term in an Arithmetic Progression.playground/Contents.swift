c51fedcb681b9aa8256ca1149a8883da9c5b66b4
Yegor Gorskikh <egorgorskikh@gmail.com>
2022-06-20 20:16:49 +0300

    feat: Find the missing term in an Arithmetic Progression

---
+++ 5-6/Find the missing term in an Arithmetic Progression.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+// Find the missing term in an Arithmetic Progression. Swift.
+
+func find_missing(l:[Int]) -> Int {
+    let sum = l.reduce(0, +)
+    return (l[0] + l[l.count-1]) * (l.count + 1) / 2 - sum
+}
