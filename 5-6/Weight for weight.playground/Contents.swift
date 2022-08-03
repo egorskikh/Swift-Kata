8ac2421be2c771ef67eaad16cac093cb25cbca47
Yegor Gorskikh <egorgorskikh@gmail.com>
2022-08-03 20:02:23 +0300

    feat: Weight for weight

---
+++ 5-6/Weight for weight.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+// Weight for weight. Swift.
+
+func orderWeight(_ s: String) -> String {
+    return s.components(separatedBy: " ")
+        .sorted {
+        let lhs = $0
+                .compactMap{ Int(String($0)) }
+                .reduce(0, +)
+            
+        let rhs = $1
+                .compactMap{ Int(String($0)) }
+                .reduce(0, +)
+        
+        return lhs == rhs ? $0 < $1 : lhs < rhs
+    }.joined(separator: " ")
+}
