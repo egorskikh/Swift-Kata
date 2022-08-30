813f450b6bba745d97f9e81d750d49c2942b2f73
Yegor Gorskikh <egorgorskikh@gmail.com>
2022-08-30 21:10:19 +0300

    feat: Disguised sequences (II)

---
+++ 5-6/Disguised sequences (II).playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+// Disguised sequences (II)
+
+func v1(_ n: Int, _ p: Int) -> Int {
+    return (2 * n + 1) * p
+}
+
+func u1(_ n: Int, _ p: Int) -> Int {
+    return (n + 1) * p
+}
